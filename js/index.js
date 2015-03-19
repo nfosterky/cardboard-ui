805b1559bd001e911a9cc66696d1862ec9d15404
Nathaniel Foster <nathanielsmusic@gmail.com>
2015-03-19 17:09:51 -0400

    added camera feed, gyro, and up/down movement

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 var INPUT_DISTANCE = 400;
 
-var scene, camera, renderer, controls, hud;
+var scene, camera, renderer, controls, hud, vObj;
 
 var cells = [];
+
+var move = {
+  up: false,
+  down: false
+};
 
 function init () {
   // create scene
@@ -45,6 +50,8 @@ function init () {
   makeCells(24, 4);
   animate();
 
+  addVideoFeed();
+  initGyro();
   // how to determine if a cell is no longer hovered over by hud?
   // var cellElems = document.getElementsByClassName("cell");
   //
@@ -139,6 +146,84 @@ function makeCell (cellIndex, width, height) {
   return new THREE.CSS3DObject( cell );
 }
 
+function addVideoFeed () {
+  var videoSource = null
+    errBack = function(error) {
+      console.log("Video capture error: ", error);
+    };
+
+  var getUserMedia = null;
+
+  if (navigator.getUserMedia) {
+    getUserMedia = function(a, b, c) {
+      navigator.getUserMedia(a, b, c);
+    }
+
+  } else if (navigator.webkitGetUserMedia) {
+    getUserMedia = function(a, b, c) {
+      navigator.webkitGetUserMedia(a, b, c);
+    }
+  }
+
+  if (typeof MediaStreamTrack !== "undefined") {
+    MediaStreamTrack.getSources(function(sourceInfos) {
+      var sourceInfo, media;
+
+      // find last video source - might need to add check, last video might not
+      // always be what we want?
+      for (var i = 0; i < sourceInfos.length; i++) {
+        sourceInfo = sourceInfos[i];
+
+        if (sourceInfo.kind === 'video') {
+          videoSource = sourceInfo.id;
+
+        } else {
+          console.log('Some other kind of source: ', sourceInfo);
+        }
+      }
+
+      // use sourceId to select either front or back camera
+      media = { video: { optional: [{ sourceId: videoSource }] } };
+
+      getUserMedia(media, function(stream) {
+        var url = window.URL.createObjectURL(stream);
+
+        var video = document.createElement( 'video' );
+
+        vObj = new THREE.CSS3DObject( video );
+
+        vObj.elementL.src = url;
+        vObj.elementR.src = url;
+
+        vObj.position.set(0, 1000, -500);
+
+        // camera.position.set(0,1000,0);
+
+        camera.add(vObj);
+
+      }, errBack);
+    });
+  }
+}
+
+function initGyro () {
+  gyro.frequency = 100;
+
+  gyro.startTracking(function(o) {
+    if (o.gamma) {
+
+      if (o.gamma > 0 && o.gamma < 45) {
+        move.up = true;
+        move.down = false;
+
+      } else if (o.gamma < 0 && o.gamma > -45) {
+        move.up = false;
+        move.down = true;
+      }
+    }
+  });
+}
+
 function animate () {
   requestAnimationFrame( animate );
 
@@ -147,6 +232,26 @@ function animate () {
   controls.update();
 
   renderer.render( scene, camera );
+
+  if (vObj && vObj.elementL.paused) {
+    vObj.elementL.play();
+  }
+
+  if (vObj && vObj.elementR.paused) {
+    vObj.elementR.play();
+  }
+
+
+  if (move.up) {
+    console.log("move up");
+    camera.position.y = 3000;
+    vObj.position.y = 0;
+
+  } else if (move.down) {
+    console.log("move down");
+    camera.position.y = 250;
+    vObj.position.y = 1000;
+  }
 }
 
 init();
