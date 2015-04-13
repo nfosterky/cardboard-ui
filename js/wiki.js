c9c378b5af0485c96f5f5a02c35f59333caeabcf
Nathaniel Foster <nathanielsmusic@gmail.com>
2015-04-13 14:23:42 -0400

    added window resizing for phone rotation

--- js/wiki.js
+++ js/wiki.js
@@ -43,6 +43,19 @@ function prep () {
       alert( "Please enter search term" );
     }
   }
+
+  window.addEventListener( 'resize', onWindowResize, false );
+}
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	render();
+
 }
 
 function init ( searchTerm ) {
